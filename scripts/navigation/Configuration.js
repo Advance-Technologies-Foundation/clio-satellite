033efe7cf497bba1ad9be63f6a0b71a1d7bd52d0
P.Shvedun <P.Shvedun@bpmonline.com>
2025-10-31 01:38:32 +0200

    Fix configuration action from buisness process

--- scripts/navigation/Configuration.js
+++ scripts/navigation/Configuration.js
@@ -15,6 +15,13 @@
     }
   }
 
+  // Remove /Nui from the path if it exists
+  let basePath = newPath[0];
+  if (basePath && basePath.includes('/Nui')) {
+    basePath = basePath.replace(/\/Nui/g, '');
+    newPath[0] = basePath;
+  }
+
   // Fixed the syntax error in the original script
   newPath.push("dev");
   window.open(newPath.join('/'), '_blank');
